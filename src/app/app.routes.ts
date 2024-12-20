55359e8af1a9754ff0d7aa96c66a61168bdbb36b
MiskaCs90 <162002700+MiskaCs90@users.noreply.github.com>
2024-12-20 21:15:04 +0100

    portfolio basis created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { PageContentComponent } from './page-content/page-content.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: PageContentComponent},
+];
---
+++ src/app/page-content/page-content.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { MySkillsComponent } from './my-skills/my-skills.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ContactComponent } from './contact/contact.component';
+
+@Component({
+  selector: 'app-page-content',
+  imports: [
+    LandingPageComponent,
+    AboutMeComponent,
+    MySkillsComponent,
+    PortfolioComponent,
+    ContactComponent,
+  ],
+  templateUrl: './page-content.component.html',
+  styleUrl: './page-content.component.scss'
+})
+export class PageContentComponent {
+
+}
