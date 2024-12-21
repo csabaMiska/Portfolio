6216719fb8417786d6c8b07aa4db7425236fc985
MiskaCs90 <miskacs90@icloud.com>
2024-12-22 00:15:43 +0100

    Landing page has been built

--- src/app/page-content/landing-page/landing-page.component.ts
+++ src/app/page-content/landing-page/landing-page.component.ts
@@ -1,8 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-landing-page',
-  imports: [],
+  imports: [
+    CommonModule,
+    MatIconModule
+  ],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
 })
