820f10e0725f58d505d7e666bd692bbc48d25a28
MiskaCs90 <miskacs90@icloud.com>
2024-12-27 13:48:15 +0100

    portfolio projects build and projects injected

--- src/app/page-content/portfolio/portfolio.component.ts
+++ src/app/page-content/portfolio/portfolio.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { PortfolioDataService } from '../../portfoliodata.service';
 
 @Component({
   selector: 'app-portfolio',
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './portfolio.component.html',
   styleUrl: './portfolio.component.scss'
 })
 export class PortfolioComponent {
+  portfolioDate = inject(PortfolioDataService);
 
+  portfolioProjects = this.portfolioDate.portfolioProjects;
 }
--- src/app/portfoliodata.service.ts
+++ src/app/portfoliodata.service.ts
@@ -48,5 +48,32 @@ export class PortfolioDataService {
       title: "Material Design",
       img: "material_design.png"
     }
-  ]
+  ];
+
+  portfolioProjects = [
+    {
+      name: 'Pokédex',
+      img: 'pokedex.png',
+      used_skills: ['Javascript', 'HTML', 'CSS', 'Api'],
+      description: 'Based on the PokéAPI a simple library that provides and catalogues pokemon information.',
+      link: 'hier kommt das link',
+      github_link: 'hier kommt das githublink'
+    },
+    {
+      name: 'Join',
+      img: 'join.png',
+      used_skills: ['Angular', 'TypeScript', 'HTML', 'CSS', 'Firebase'],
+      description: 'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories.',
+      link: 'hier kommt das link',
+      github_link: 'hier kommt das githublink'
+    },
+    {
+      name: 'Sharkie',
+      img: 'sharkie.png',
+      used_skills: ['Javascript', 'HTML', 'CSS',],
+      description: 'A simple Jump-and-Run game based on an object-oriented approach. Help sharkie to find coins and poison bottles to fight against the killer whale.',
+      link: 'hier kommt das link',
+      github_link: 'hier kommt das githublink'
+    }
+  ];
 }
